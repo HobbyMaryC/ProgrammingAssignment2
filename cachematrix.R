91e94d70b84d664643cc814b1506248bcb34cac2
Mary Chou <hobbymaryc@gmail.com>
2015-07-26 16:28:27 -0500

    Fixed comments and printing.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Assignment 2:  Inverse caching: use free variable, super assignment operator
 ## 
 
-## Creates a special matrix object that can cache its inverse.
+# Creates a special matrix object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -16,23 +16,22 @@ makeCacheMatrix <- function(x = matrix()) {
        getinv = getinv)
 }
 
+# Return a matrix that is the inverse of 'x'
 # Computes the inverse of the special matrix returned by makeCacheMatrix().
 # If the inverse has been calculated, and the matrix has not changed,
 # returns the inverse from the cache.
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
   inv2 <- x$getinv()
   if (!is.null(inv2)) {
     message("getting cached data")
     print("access cache")
-    inv2				# Opt to print the value also			
+    print(inv2)				# Opt to print the value also			
     return(inv2)
   }
   print("calculating inverse")
   cache.matrix <- x$get()
   inv2 <- solve(cache.matrix)
   x$setinv(inv2)
-  inv2					# Opt to print the value also
-  return(inv2)
+  print(inv2)					# Opt to print the value also
+  inv2                # return is optional
 }
